1c7c0f9da4f82e87e958d1fb3d7c4ba825a59629
Evelina Norlin <evelina.norlin@hotmail.com>
2023-09-05 10:59:16 +0200

    added context frot restaurantId

--- Restaurangen/src/App.tsx
+++ Restaurangen/src/App.tsx
@@ -1,32 +1,18 @@
 
 import { RouterProvider } from 'react-router-dom'
-import { createRestaurant, getRestaurant } from './services/restaurantServices'
 
 import './App.css'
 import { router } from './Router'
+import { restaurantIdContext } from './contexts/restaurantIdContext'
 
 function App() {
   const restaurantId = '64f6d9c5264e4838c0d69d38';
 
-  const restaurant = getRestaurant(restaurantId);
-
-  console.log(restaurant);
-
-  // createRestaurant({
-  //   "name": "Hamburgerian",
-  //   "address": {
-  //     "street": "Hamngatan 1",
-  //     "zip": "123 45",
-  //     "city": "Dalsjöfors"
-  //   }
-  // })
-
-
-
-
   return (
     <>
+      <restaurantIdContext.Provider value={restaurantId}>
       <RouterProvider router={router}></RouterProvider>
+      </restaurantIdContext.Provider>
     </>
   )
 }
