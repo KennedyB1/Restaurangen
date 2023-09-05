c7626aa16a42664a63f8e0774df9d1777a1ac45b
Therese <therese.holen22@gmail.com>
2023-09-05 09:12:20 +0200

    Added Router and Components

---
+++ Restaurangen/src/Router.tsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter} from "react-router-dom";
+import { Layout } from "./Components/Layout";
+import { Home } from "./Components/Home";
+import { About } from "./Components/About";
+import { Menu } from "./Components/Menu";
+import { Booking } from "./Components/Booking";
+import { Admin } from "./Components/Admin";
+
+export const router = createBrowserRouter([
+    {
+        path:"/",
+        element:<Layout></Layout>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>,
+                index: true
+            },
+            {
+                path: "/om",
+                element: <About></About>
+            },
+            {
+                path: "/meny",
+                element: <Menu></Menu>
+            },
+            {
+                path: "/bokning",
+                element: <Booking></Booking>
+            },
+            {
+                path: "/admin",
+                element: <Admin></Admin>
+            },
+        ]
+    }
+]) 
