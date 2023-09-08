ab522ce3bee1593f52a6bd8243d1e51d7b85acd2
Evelina Norlin <evelina.norlin@hotmail.com>
2023-09-08 11:21:43 +0200

    One can search bookings based on date

--- Restaurangen/src/Components/Dashboard.tsx
+++ Restaurangen/src/Components/Dashboard.tsx
@@ -1,69 +1,68 @@
-import { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { restaurantIdContext } from "../contexts/restaurantIdContext";
-import { getBookings } from "../services/restaurantServices";
+import { getBookings, getCostumer } from "../services/restaurantServices";
 import { ICostumer, IFetchedBooking } from "../interfaces/interfaces";
 import { BookingSection } from "./style/BookingStyle";
 import { BookingWrapper } from "./style/Wrappers";
 import { H2centered } from "./style/Title";
 import Datepicker from "./DatePicker";
 import { LightP } from "./style/P";
-import { getCostumer } from "../services/restaurantServices";
 
 export const Dashboard = () => {
-    const restaurantId = useContext(restaurantIdContext);
-    const [bookings, setBookings] = useState<IFetchedBooking[]>([]);
-    const [bookingDate, setBookingDate] = useState<Date>(new Date());
-    const [foundBookings, setFoundBookings] = useState<IFetchedBooking[]>([]);
-    const [costumers, setCostumers] = useState<ICostumer[]>([]);
-    let costumersArr: ICostumer[] = [];
-    
-    // Use a single `useEffect` to fetch bookings and update `bookings` state.
-    useEffect(() => {
-        async function fetchBookings() {
-            const bookings = await getBookings(restaurantId);
-            setBookings(bookings);
-        }
-        fetchBookings();
-    }, [restaurantId]);
+  const restaurantId = useContext(restaurantIdContext);
+  const [bookings, setBookings] = useState<IFetchedBooking[]>([]);
+  const [bookingDate, setBookingDate] = useState<Date>(new Date());
+  const [foundBookings, setFoundBookings] = useState<IFetchedBooking[]>([]);
+  const [costumers, setCostumers] = useState<ICostumer[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false); // Loading state
 
-    // Use a single `useEffect` to filter and update `foundBookings` state.
-    useEffect(() => {
-        const filteredBookings = bookings.filter((booking) => {
-            return booking.date === bookingDate.toISOString().slice(0, 10);
-        });
+  useEffect(() => {
+    async function fetchData() {
+      setIsLoading(true); // Set loading to true when fetching data
+      const bookingsData = await getBookings(restaurantId);
+      setBookings(bookingsData);
+      setIsLoading(false); // Set loading to false when data is fetched
+    }
 
-        setFoundBookings(filteredBookings);
+    fetchData();
+  }, [restaurantId]);
 
-        getCostumersArr();
-        
-    }, [bookingDate, bookings]); // Include `bookings` as a dependency here
-    
-    // loopa igenom foundbookings för att hitta alla kunders namn
-    const getCostumersArr = async () => {
-        costumersArr = await Promise.all(foundBookings.map(async (booking) => {
-            const costumer = await getCostumer(booking.customerId);
-            return costumer;
-        }));
-        console.log(costumersArr);
-        setCostumers(costumersArr);
-        console.log(costumers);
-        }
+  useEffect(() => {
+    async function getCostumersArr() {
+      const filteredBookings = bookings.filter((booking) => {
+        return booking.date === bookingDate.toISOString().slice(0, 10);
+      });
 
-        costumersArr.map((booking) => () => {
-            console.log(booking.name)
-        })
+      setIsLoading(true); // Set loading to true when fetching data
+      const costumerPromises = filteredBookings.map(async (booking) => {
+        const costumerArray = await getCostumer(booking.customerId);
+        return costumerArray[0];
+      });
+      
+      const costumersData = await Promise.all(costumerPromises);
+      setCostumers(costumersData);
+      setIsLoading(false); // Set loading to false when data is fetched
+    }
 
+    getCostumersArr();
+  }, [bookingDate, bookings]);
 
-    return (
-        <BookingSection>
-            <BookingWrapper>
-                <H2centered>Välj datum för att visa bokningar</H2centered>
-                <Datepicker setDate={setBookingDate} date={bookingDate} />
-                {
-                costumers.map((booking) => (
-                    <LightP key={booking.id}>{booking.name}</LightP>
-                ))}
-            </BookingWrapper>
-        </BookingSection>
-    );
+  return (
+    <BookingSection>
+      <BookingWrapper>
+        <H2centered>Välj datum för att visa bokningar</H2centered>
+        <Datepicker setDate={setBookingDate} date={bookingDate} />
+        {isLoading ? (
+          <p>Loading...</p> // Display loading indicator when isLoading is true
+        ) : (
+          costumers.map((costumer) => (
+            <LightP key={costumer.id}>
+              {costumer.name} {costumer.lastname}
+            </LightP>
+          ))
+        )}
+      </BookingWrapper>
+    </BookingSection>
+  );
 };
+
